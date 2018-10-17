3ac749f07e8e9f39fed6411d60bd308e961b7df0
MegaChuck64 <cscarey@email.neit.edu>
2018-10-17 03:48:23 -0400

    Don't need because it is now stored on my github pages
    megachuck64.github.io

--- Project/bookmarklet.js
+++
@@ -1,1 +0,0 @@
-(function(){console.log("bookmarklet starting");let paragraphs = document.getElementsByTagName('p');for (let i = 0; i < paragraphs.length; i++){paragraphs[i].innerHTML = 'GOD';}})();
